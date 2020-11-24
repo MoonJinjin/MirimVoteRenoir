04d82a7e007f7505e6c7858605b348db31f0c15f
MoonJinjin <s2019s11@e-mirim.hs.kr>
2020-11-24 20:01:17 +0900

    Add Result Image

--- app/src/main/java/kr/hs/emirim/s2019s11/mirimvoterenoir/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s11/mirimvoterenoir/ResultActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -21,9 +22,11 @@ public class ResultActivity extends AppCompatActivity {
         String[] imgNames = intent.getStringArrayExtra("imgNames");
         TextView[] textVs = new TextView[imgNames.length];
         RatingBar[] ratingBars = new RatingBar[imgNames.length];
+        ImageView imgResult = findViewById(R.id.img_result);
 
         int[] textIds = {R.id.text_pic1, R.id.text_pic2, R.id.text_pic3, R.id.text_pic4, R.id.text_pic5, R.id.text_pic6, R.id.text_pic7, R.id.text_pic8, R.id.text_pic9 };
         int[] ratingIds = {R.id.rating_bar1, R.id.rating_bar2, R.id.rating_bar3, R.id.rating_bar4, R.id.rating_bar5, R.id.rating_bar6, R.id.rating_bar7, R.id.rating_bar8, R.id.rating_bar9 };
+        Integer imageFileId[] = {R.drawable.vote1, R.drawable.vote2, R.drawable.vote3, R.drawable.vote4, R.drawable.vote5, R.drawable.vote6, R.drawable.vote7, R.drawable.vote8, R.drawable.vote9};
 
         for(int i=0; i<imgNames.length; i++) {
             textVs[i] = findViewById(textIds[i]);
@@ -31,6 +34,8 @@ public class ResultActivity extends AppCompatActivity {
 
             textVs[i].setText(imgNames[i]);
             ratingBars[i].setRating((float)voteCount[i]);
+
+            imgResult.setImageResource(imageFileId[i]);
         }
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +44,6 @@ public class ResultActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 }
